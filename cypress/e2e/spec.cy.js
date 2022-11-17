3283424ff3d5acdbdeb9c3858720f0a0dfe56558
ZHShen7 <1145720179@qq.com>
2022-11-17 11:34:18 +0800

    cypress-githubAction test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,6 @@
 
 describe('homePage test', () => {
-  before(() => {
-    cy.visit('localhost:3000/contents/homePage')
-    cy.viewport(320, 480)
-    cy.viewport('iphone-5')
-  })
-  beforeEach(() => {
-  })
-  it('正确显示首页内容', () => {
-    cy.get('.footer_icon___WayJ').first().should('contain','')
+  it('test',()=>{
+    cy.visit('www.baidu.com')
   })
 })
