03713f1ae53a2188ba8c0cf2bff70cb6d4e55ad8
ZHShen7 <1145720179@qq.com>
2022-11-20 19:05:34 +0800

    github actions test

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  // projectId: "76xo3p",
+  projectId: "76xo3p",
   e2e: {
     setupNodeEvents(on, config) {
-      
+
     },
     baseUrl: "http://localhost:3000",
     supportFile: "test/cypress/support/e2e.{js,jsx,ts,tsx}",
     specPattern: "test/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}"
   },
-  // fixturesFolder: "test/cypress/fixtures",
-  // videosFolder: "test/cypress/videos",
+  fixturesFolder: "test/cypress/fixtures",
+  videosFolder: "test/cypress/videos",
 });
