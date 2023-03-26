29f694d750d7e69760e990b022b5185d011f50e4
ZHShen7 <74863858+ZHShen7@users.noreply.github.com>
2023-03-26 15:57:57 +0800

    Update socket.js

--- models/socket.js
+++ models/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io('http://47.109.50.153:3001');
 console.log('建立socket链接')
 
 export default socket;
